61c94ffd6f11e5e98ea9af1576a4e7515037b5e4
huangwenbo <nufedong@163.com>
2022-08-04 14:18:12 +0800

    3 修改redis配置,添加菜品缓存

--- src/main/java/reggie/controller/UserController.java
+++ src/main/java/reggie/controller/UserController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import reggie.common.R;
 import reggie.entity.User;
@@ -13,6 +14,7 @@ import reggie.utils.ValidateCodeUtils;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author hwbstart
@@ -25,15 +27,18 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //添加redis对象
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 获取验证码
      *
      * @param user
-     * @param session
+
      * @return
      */
     @PostMapping("/sendMsg")
-    public R<String> sendMsg(@RequestBody User user, HttpSession session) {
+    public R<String> sendMsg(@RequestBody User user) {
         //获取手机号
         String phone = user.getPhone();
         //判断手机号是否为空
@@ -44,7 +49,9 @@ public class UserController {
             //调用API发送信息
             //SMSUtils.sendMessage("阿里云", "", phone, s);
             //将验证码存入session中
-            session.setAttribute(phone, s);
+            //session.setAttribute(phone, s);
+            //将生成的验证码存入redis中
+            redisTemplate.opsForValue().set(phone,s,1, TimeUnit.MINUTES);
             return R.success("验证码发送成功！");
         }
         return R.error("发送失败!手机号为空！");
@@ -68,10 +75,11 @@ public class UserController {
         String code = map.get("code").toString();
 
         //从Session中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone);
-
-        //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
-        if (codeInSession != null && codeInSession.equals(code)) {
+        //Object codeInSession = session.getAttribute(phone);
+        //从redis中获取验证码
+        String o = (String) redisTemplate.opsForValue().get(phone);
+        //进行验证码的比对（页面提交的验证码和redis中保存的验证码比对）
+        if (o != null && o.equals(code)) {
             //如果能够比对成功，说明登录成功
 
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
